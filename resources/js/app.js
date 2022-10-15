eb843de7680180f74e82dfe44844ce7515dd063e
andersen51205 <andersen51205@gmail.com>
2022-10-15 11:37:20 +0800

    SweetAlert2套件配置

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,11 @@ import './bootstrap';
 
 // Sweet Alert 套件引入
 window.Swal = require('sweetalert2');
+// 配置套件
+window.Swal = Swal.mixin({
+    allowOutsideClick: false,
+    confirmButtonText: '關閉',
+});
 
 // Chart.js 套件引入
 window.Chart = require('chart.js/auto').default;
