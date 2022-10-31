b9c8c4f10c629a9d4e682d5403a5edf63d4da6c8
andersen51205 <andersen51205@gmail.com>
2022-10-31 16:15:58 +0800

    分類管理 Model 建立

---
+++ database/migrations/2022_10_28_074952_create_categories_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->integer('type');
+            $table->integer('icon');
+            $table->integer('color');
+            $table->string('name', 100);
+            $table->integer('sort');
+            $table->boolean('hidden')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
